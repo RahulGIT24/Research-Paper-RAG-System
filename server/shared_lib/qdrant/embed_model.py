751a0edd21ed05f6de86edfc3b896256217926e6
rahul <rg4005450@gmail.com>
2026-07-18 01:43:30 +0530

    Dockerized and hf model cached finally

--- server/shared_lib/qdrant/embed_model.py
+++ server/shared_lib/qdrant/embed_model.py
@@ -1,8 +1,14 @@
+import os
 from llama_index.embeddings.fastembed import FastEmbedEmbedding
+
+FASTEMBED_CACHE_DIR = os.path.join(
+    os.path.expanduser("~"), ".cache", "huggingface", "fastembed"
+)
 
 class EmbedModel:
     @staticmethod
     def get_embed_model(model="BAAI/bge-small-en-v1.5"):
         return FastEmbedEmbedding(
-    model_name=model
-)
+            model_name=model,
+            cache_dir=FASTEMBED_CACHE_DIR
+        )
